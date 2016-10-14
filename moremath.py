ede7d9723eaee87d5d3bef8eb6ed7df1223cb98e
Alex <Haxalotify@users.noreply.github.com>
2016-10-14 10:41:45 -0700

    Add files via upload

---
+++ moremath.py
@@ -0,0 +1,41 @@
+import math
+
+#rounds number up to 2
+round(1.5)
+
+#rounds number down
+math.floor(1.5)
+
+#rounds number up
+math.ceil(1.5)
+
+#basic trig
+math.sin(math.pi / 2) # = 1
+
+math.sin(math.pi)#gives really big float number to e-16 power
+
+#same as up above but using the round function
+math.floor(math.sin(math.pi))
+
+math.cos(0)# = 1
+
+math.asin(0)# = 0.0
+
+math.acos(0)# = float number
+
+#hypotenuse of right angle triangle
+math.hypot(3,4)#=5
+
+#is 2 to the power of 3 = 8
+math.pow(2,3)
+
+math.pow(9,2)
+9 ** 2
+
+#exponention of a number
+math.exp(2)
+
+#naturallogs
+math.log(math.e)# = 1
+math.log10(1000)#10 to the power of 3 is 1000
+math.log2(8)#2 to the power of 3 is 8, 2 to the what power = 8?
