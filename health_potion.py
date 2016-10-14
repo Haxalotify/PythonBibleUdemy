be312f4c56f9f8901f0ff8da24651d9f105ba221
Alex <Haxalotify@users.noreply.github.com>
2016-10-14 10:26:54 -0700

    Add files via upload
    added difficulty system to set how much health the potion gives the player based on the difficulty of the game; easy, medium, hard

--- health_potion.py
+++ health_potion.py
@@ -2,4 +2,13 @@ import random
 
 health = 50
 
-potion_health = random.randint(25, 50)
+#creating a difficulty, choose between 1, 2, 3. Higher the difficulty
+#the less health potion_health will give
+difficulty = 1
+
+#int, converts to an integer instead of float known as casting
+potion_health = int(random.randint(25, 50) / difficulty)
+
+#using a potion to increase player health
+health = health + potion_health
+print(health)
